e52e91da6e5a3c2e65c0d98c8738042ec0e6a4ff
lucianlorens <lucianlorens@gmail.com>
2022-09-18 20:54:43 +0100

    :sparkles: add next_page urljoiner for getting to the next page in crawling.

--- webcrawling/scrapy_tutorial/tutorial/tutorial/spiders/quotes_spider.py
+++ webcrawling/scrapy_tutorial/tutorial/tutorial/spiders/quotes_spider.py
@@ -8,10 +8,18 @@ class QuotesSpider(scrapy.Spider):
     ]
     
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = f'quotes-{page}.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log(f'Saved file {filename}')
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').get(),
+                'author': quote.css('small.author::text').get(),
+                'tags': quote.css('div.tags a.tag::text').getall(),
+            }
+        
+        next_page = response.css('li.next a').attrib['href']
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
+        
+
 
     
